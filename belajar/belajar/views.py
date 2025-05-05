6768006b2a4984bfe12a9f53fa880eb9a178979c
eka syarif <eka.ckp16799@github.com>
2025-05-05 16:11:20 +0700

    p

---
+++ belajar/belajar/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the belajar index.")
